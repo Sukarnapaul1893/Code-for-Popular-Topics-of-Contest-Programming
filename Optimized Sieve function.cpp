bfd6673d14b9e3685e6b44bb7eecdbb4275d575a
Sukarnapaul1893 <32437932+Sukarnapaul1893@users.noreply.github.com>
2019-02-22 18:45:20 +0600

    Update Optimized Sieve function.cpp

--- Optimized Sieve function.cpp
+++ Optimized Sieve function.cpp
@@ -24,12 +24,12 @@ void sieve ()
 {
 	bs.set ();
         prime.pb(2LL);
-	for (ll i = 3; i < 10000000; i+=2)
+	for (ll i = 3; i <= 10000000; i+=2)
 	{
 		if (bs[i])
 		{
 			prime.pb(i);
-			for (ll j = i * i; j < 10000000; j += i+i)
+			for (ll j = i * i; j <= 10000000; j += i+i)
 			{
 				bs[j] = 0;
 			}
