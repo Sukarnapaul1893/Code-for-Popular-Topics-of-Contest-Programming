746784b5fdf3660c6a5ec111f4ddb5332cfd7f37
Sukarna Paul <sukarnapaul1893@gmail.com>
2021-01-04 13:53:53 +0600

    Update MOD.cpp

--- Math/MOD.cpp
+++ Math/MOD.cpp
@@ -1,24 +1,16 @@
-inline void normal(ll& a) {
-	a %= mod;
-	(a < 0) && (a += mod);
-}
-
 inline ll mul(ll a, ll b) {
 	a %= mod, b %= mod;
-	normal(a), normal(b);
 	return (a * b) % mod;
 }
-
+ 
 inline ll add(ll a, ll b) {
 	a %= mod, b %= mod;
-	normal(a), normal(b);
 	return (a + b) % mod;
 }
-
+ 
 inline ll sub(ll a, ll b) {
 	a %= mod, b %= mod;
-	normal(a), normal(b);
 	a -= b;
-	normal(a);
+	a+=mod; a%=mod;
 	return a;
 }
