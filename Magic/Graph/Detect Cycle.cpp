d6248cbb679b4bdae6f62b951db8c394be2d5fed
Sukarna Paul <sukarnapaul1893@gmail.com>
2021-01-18 21:14:23 +0600

    Create Detect Cycle.cpp

---
+++ Magic/Graph/Detect Cycle.cpp
@@ -0,0 +1,26 @@
+//detect cycle
+
+/* initially all vertices are colored 0. 
+   Now when dfs starts from some vertex, we will color it to 1.
+   Then when dfs ends from that vertex , we will color it to 2.
+   If at some point, we come to some vertex which is already colored by 1, it will mean that there is a cycle.
+*/
+
+#define MAXN 100005
+
+vector<int>adj[MAXN];
+int color[MAXN];
+bool cycle;         // initialize it with false
+
+void dfs(int v){
+    if(color[v] == 0){
+        color[v] = 1;
+        for(auto u : adj[v]){
+            dfs(u);
+        }
+        color[v] = 2;
+    }
+    else if(color[v] == 1){
+        cycle = true;
+    }
+}
