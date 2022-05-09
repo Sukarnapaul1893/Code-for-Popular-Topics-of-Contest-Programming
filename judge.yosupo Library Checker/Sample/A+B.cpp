352b3fa201ea11c45921cb66b9b9c1b0f7e653ca
Sukarna Paul <sukarnapaul1893@gmail.com>
2022-05-09 22:03:52 +0600

    Create A+B.cpp

---
+++ judge.yosupo Library Checker/Sample/A+B.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+
+using namespace std;
+
+ll a,b;
+
+void Input();
+
+void Solve(){
+    cout<<a+b<<"\n";
+}
+
+void Clear(){
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    Input();
+    Solve();
+    Clear();
+    
+}
+
+void Input(){
+    cin>>a>>b;
+}
